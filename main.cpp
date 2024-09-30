8bf7832c2eef39b47a59e28d75212845a7fecfb2
OonoYouji <k023g0007@g.neec.ac.jp>
2024-09-30 11:17:24 +0900

    関数テンプレートをしようして引数の最小値を返す関数を作成。 char型のみ例外で比較しない

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <stdio.h>
+#include <format>
 
+
+
+template <typename T>
+T Min(T a, T b) {
+	return a < b ? a : b;
+}
+
+template<>
+char Min(char a, char b) {
+	printf("数字以外は代入できません\n");
+	return 0;
+}
 
 
 int main() {
 
-	///
-	/// ↓変数の宣言
-	///
+	printf(std::format("int    min = {}\n", Min<int>(4, 5)).c_str());
+	printf(std::format("float  min = {}\n", Min<float>(2.0f, 0.4f)).c_str());
+	printf(std::format("double min = {}\n", Min<double>(3.14, 0.35)).c_str());
 
+	Min<char>('a', 'b');
 
-	///
-	/// ↑変数の宣言
-	///
-	
-	printf("hello world\n");
-	printf("大ノようじ\n");
-
-	
-	
 	return 0;
 }
 
