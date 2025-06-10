bbd867b2f56f2e99871c5e48d97c9b4063df3c68
OonoYouji <k023g0007@g.neec.ac.jp>
2025-06-11 01:09:20 +0900

    ecsを削除

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 
-#include "project/ecs/ECS.h"
-
-class Player : public IEntity {};
-class Enemy : public IEntity {};
-
-class Transform : IComponent {};
-class Collider : IComponent {};
 
 int main() {
-	ECS ecs;
-
-	Player* player = ecs.generateEntity<Player>();
-	Enemy* enemy = ecs.generateEntity<Enemy>();
-
-	player->AddComponent<Transform>();
-	enemy->AddComponent<Transform>();
-	player->AddComponent<Collider>();
 
 	return 0;
 }
