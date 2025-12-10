dab02e98024e98bee36967e5803df15bf1f2984b
OonoYouji <k023g0007@g.neec.ac.jp>
2025-12-10 19:05:23 +0900

    komi

--- main.cpp
+++ main.cpp
@@ -1,22 +1,60 @@
 #include <stdio.h>
+#include <vector>
+#include <functional>
 
+class A {
+public:
+	void Show() {
+		printf("A::Show()\n");
+	}
+};
+
+class B {
+public:
+	void Demo() {
+		printf("B::Demo()\n");
+	}
+};
+
+class C {
+public:
+	void Show() {
+		printf("C::Show()\n");
+	}
+};
+
+template<class T>
+concept Showable = requires(T t) {
+	t.Show();
+};
+
+class ShowManager {
+	std::vector<std::function<void()>> funcs_;
+
+public:
+	void Add(Showable auto* obj) {
+		funcs_.push_back([obj]() { obj->Show(); });
+	}
+
+	void CallAll() {
+		for (auto& f : funcs_) f();
+	}
+};
 
 
 int main() {
+	ShowManager manager;
 
-	///
-	/// ↓変数の宣言
-	///
+	A a;
+	B b;
+	C c;
 
+	manager.Add(&a);
+	manager.Add(&b);
+	manager.Add(&c);
 
-	///
-	/// ↑変数の宣言
-	///
-	
+	manager.CallAll();
 
-
-	
-	
 	return 0;
 }
 
